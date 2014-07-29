fb56497349ff69e54bf1caf967c8d236874eb480
gmsantosxl <gabriel.ms1@hotmail.com>
2014-07-29 00:04:09 -0300

    [02.Datatypes] SimpleXML - Xpath

--- PHP_Cert/PHPConsoleProject/02.Data types/SimpleXML.php
+++ PHP_Cert/PHPConsoleProject/02.Data types/SimpleXML.php
@@ -1,3 +1,23 @@
 <?php
 
-?>
+$simpleXml = new SimpleXMLElement('XmlFileValid.xml', 0, true);
+
+// procedural call
+//$simpleXml = simplexml_load_file('XmlFileValid.xml');
+
+// Invalid XML file display warnings
+//$invalidXml = simplexml_load_file('XmlFileInvalid.xml');
+
+/**
+ * Xpath
+ * 
+ * - Always return an Array
+ * - Node full path (/<parentNode>/<childNode>)
+ * - Can omit node match ( // is same as /* /) 
+ */ 
+
+$booksInTitleOrder = $simpleXml->xpath('///title');
+
+foreach ($booksInTitleOrder as $title){
+    echo $title . PHP_EOL;
+}
