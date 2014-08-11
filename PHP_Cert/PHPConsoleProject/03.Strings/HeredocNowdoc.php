464044c02264b560525e49584dc846b26d7c892a
gmsantosxl <gabriel.ms1@hotmail.com>
2014-08-11 17:01:54 -0300

    03. Strings (Work in Progress)

---
+++ PHP_Cert/PHPConsoleProject/03.Strings/HeredocNowdoc.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Heredoc do var interpolation - Act like double quotes ""
+ * Nowdoc doesn't var interpolation - Act like single quotes ''
+ */
+
+$url = 'https://www.youtube.com/watch?v=LH5ay10RTGY';
+
+// Heredoc can omit ""
+$heredocString = <<<IDENTIFIER
+Hey, I'm a banana!!
+
+$url
+
+IDENTIFIER;
+
+$nowdocString = <<<'IDENTIFIER'
+Hey, I'm a banana!!
+
+$url
+
+// Ending must not use quotes
+'IDENTIFIER';
+
+IDENTIFIER;
+
+var_dump($heredocString);
+var_dump($nowdocString);
