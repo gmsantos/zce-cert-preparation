6c19fea14d0b215358227ca4eec377b8489454fa
gmsantosxl <gabriel.ms1@hotmail.com>
2014-08-12 22:37:11 -0300

    [03.Strings] Aditional learning resources

--- PHP_Cert/PHPConsoleProject/03.Strings/Printf.php
+++ PHP_Cert/PHPConsoleProject/03.Strings/Printf.php
@@ -1,1 +1,18 @@
 <?php
+
+// printf  - "f" from formated
+// sprintf - return formated "s"tring
+// vprintf - return output from array "v"alues
+// sscanf  - Oposit of sprintf
+// fscanf  - Scan from a File
+
+/**
+ * Arguments:
+ * 
+ * %b - Binary number
+ * %d - Decimal
+ * %f - Floar
+ * %o - Octal
+ * %e - Scientific Notation
+ * %s - String    
+ */
