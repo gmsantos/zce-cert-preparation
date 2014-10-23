7931bcb8cb2761cf5a22aac4406aa85f5b40fbd9
gmsantosxl <gabriel.ms1@hotmail.com>
2014-10-23 11:48:37 -0200

    Add more examples

---
+++ 02.Data types/DomDocument.php
@@ -0,0 +1,38 @@
+<?php
+
+// Set the content type to be XML, so that the browser will   recognise it as XML.
+header( "content-type: application/xml; charset=UTF-8" );
+
+// "Create" the document.
+$xml = new DOMDocument( "1.0", "UTF-8" );
+
+// Create some elements.
+$xml_album = $xml->createElement( "Album" );
+$xml_track = $xml->createElement( "Track", "The ninth symphony" );
+
+// Set the attributes.
+$xml_track->setAttribute( "length", "0:01:15" );
+$xml_track->setAttribute( "bitrate", "64kb/s" );
+$xml_track->setAttribute( "channels", "2" );
+
+// Create another element, just to show you can add any (realistic to computer) number of sublevels.
+$xml_note = $xml->createElement( "Note", "The last symphony composed by Ludwig van Beethoven." );
+
+// Append the whole bunch.
+$xml_track->appendChild( $xml_note );
+$xml_album->appendChild( $xml_track );
+
+// Repeat the above with some different values..
+$xml_track = $xml->createElement( "Track", "Highway Blues" );
+
+$xml_track->setAttribute( "length", "0:01:33" );
+$xml_track->setAttribute( "bitrate", "64kb/s" );
+$xml_track->setAttribute( "channels", "2" );
+$xml_album->appendChild( $xml_track );
+
+$xml->appendChild( $xml_album );
+
+// Parse the XML.
+print $xml->saveXML();
+
+$xml->save('files/DomXml.xml');
--- 03.Strings/Regex.php
+++ 03.Strings/Regex.php
@@ -1,2 +1,38 @@
 <?php
 
+$pattern = "/(\w*)([0-9]{1})\s/"; // A pattern always have a delimitator, usually '/' or '#'
+
+$string = "PHP5 released PHP6 not_released";
+
+// Return number of matches
+$numberOfMatches = preg_match_all($pattern, $string, $matches); // Third parameter is a reference to matches
+
+var_dump($matches);
+
+/*
+
+array(2) {
+  [0]=>							Whole match
+  array(2) {
+    [0]=>
+    string(5) "PHP5 "
+    [1]=>
+    string(5) "PHP6 "
+  }
+  [1]=>							Groups ()
+  array(2) {
+    [0]=>
+    string(3) "PHP"
+    [1]=>
+    string(3) "PHP"
+  } 
+  [2]=>				
+  array(2) {
+    [0]=>
+    string(1) "5"
+    [1]=>
+    string(1) "6"
+  }
+}
+
+*/
