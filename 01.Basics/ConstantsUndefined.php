a758670ea9471da57b1cb18f8d5763bea41ee06c
gmsantos <gabriel.ms1@hotmail.com>
2017-02-16 09:41:45 -0200

    Remove old files

--- 01.Basics/ConstantsUndefined.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// PHP tries to convert undefined constants to strings
-//define('MINHA_CONSTANTE', 'teste123');
-
-echo MINHA_CONSTANTE;
--- 03.Strings/str_comp.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-var_dump(strcmp('Teste', 'batata'));
-
-
